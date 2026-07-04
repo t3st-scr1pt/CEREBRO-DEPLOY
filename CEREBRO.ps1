3f66e9445e177dd3173bfcf621d0d833357ea4d6
t3st-scr1pt <t3st.scr1pt@gmail.com>
2026-07-04 13:20:57 +0000

    Primer script CEREBRO

--- CEREBRO.ps1
+++ CEREBRO.ps1
@@ -1,6 +1,44 @@
-Add-Type -AssemblyName PresentationFramework
+# ===================
+# CEREBRO DEPLOY
+# ===================
 
-[System.Windows.MessageBox]::Show(
-"CEREBRO DEPLOY FUNCIONA",
-"CEREBRO"
-)
+winget install Google.Chrome --silent
+winget install Google.Drive --silent
+winget install Tailscale.Tailscale --silent
+winget install RustDesk.RustDesk --silent
+winget install VideoLAN.VLC --silent
+winget install 7zip.7zip --silent
+
+# Modo oscuro
+
+New-ItemProperty `
+-Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize `
+-Name AppsUseLightTheme `
+-Value 0 `
+-PropertyType DWord `
+-Force
+
+New-ItemProperty `
+-Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize `
+-Name SystemUsesLightTheme `
+-Value 0 `
+-PropertyType DWord `
+-Force
+
+# Explorador
+
+Set-ItemProperty `
+-Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced `
+-Name HideFileExt `
+-Value 0
+
+Set-ItemProperty `
+-Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced `
+-Name Hidden `
+-Value 1
+
+# Energía
+
+powercfg /hibernate off
+powercfg /change standby-timeout-ac 0
+powercfg /change monitor-timeout-ac 0
